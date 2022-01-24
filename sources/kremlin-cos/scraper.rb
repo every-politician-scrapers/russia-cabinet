012d8659fd883c448eb28e7be203155202f149dd
Tony Bowden <tony@tmtm.com>
2022-01-24 19:50:42 +0200

    compare enwiki Kremlin COS

---
+++ sources/kremlin-cos/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no img name dates].freeze
+    end
+
+    def tds
+      noko.css('td,th')
+    end
+
+    def raw_combo_date
+      super.gsub(/\(.*?\)/, '').gsub('20—22 December 2011', '20 December 2011—22 December 2011').tidy
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
