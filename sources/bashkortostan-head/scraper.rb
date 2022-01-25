93c8561c7d3d51ed6b6d715a20a5cb426028a306
Tony Bowden <tony@tmtm.com>
2022-01-25 08:00:34 +0200

    compare enwiki Bashkortostan Heads

---
+++ sources/bashkortostan-head/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no term img name start end].freeze
+    end
+
+    def raw_start
+      super rescue binding.pry
+    end
+
+    def tds
+      noko.css('td,th')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
