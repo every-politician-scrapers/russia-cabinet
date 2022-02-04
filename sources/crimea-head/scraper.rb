c43bfcdef43537c5074dbe0f493c3b486e1403a0
Tony Bowden <tony@tmtm.com>
2022-02-04 12:26:36 +0200

    compare enwiki Head of Crimea

---
+++ sources/crimea-head/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Election'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no img _ name start end].freeze
+    end
+
+    def raw_end
+      super.gsub(/\(.*?\)/, '').tidy
+    end
+
+    def tds
+      noko.css('td,th')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
