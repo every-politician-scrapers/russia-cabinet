af99a703a1d2a74c46303be9b53efdf1d0491852
Tony Bowden <tony@tmtm.com>
2022-02-04 11:57:26 +0200

    compare enwiki Head of the Republic of Buryatia

---
+++ sources/buryatia-head/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Image'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no img name _ party dates].freeze
+    end
+
+    def raw_combo_date
+      super.gsub('from ', '').tidy
+    end
+
+    def tds
+      noko.css('td,th')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
