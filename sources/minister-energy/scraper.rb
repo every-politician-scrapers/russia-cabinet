859cc6e89d222af7b07d57f6629fbb0fe928ceee
Tony Bowden <tony@tmtm.com>
2022-02-27 10:37:43 +0200

    compare enwiki Energy Ministers

---
+++ sources/minister-energy/scraper.rb
@@ -0,0 +1,29 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Term'
+  end
+
+  def table_number
+    'position()>=4'
+  end
+
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[_ img name party start end].freeze
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
