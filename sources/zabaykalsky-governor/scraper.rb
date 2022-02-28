3e004d2c1d4314e4059b0aa8d50dec7cf5e23432
Tony Bowden <tony@tmtm.com>
2022-02-28 12:23:51 +0200

    collapse consecutive mandates
    This really needs to be factored out a lot better, but I'm going to wait
    until I've seen it in action across a lot more examples first.

--- sources/zabaykalsky-governor/scraper.rb
+++ sources/zabaykalsky-governor/scraper.rb
@@ -13,9 +13,22 @@ class OfficeholderList < OfficeholderListBase
     'Notes'
   end
 
+  # collapse consecutive mandates
+  def members
+    super.group_by { |row| row.values_at(:item, :itemLabel) }.map do |person, mandates|
+      mandates.flat_map { |mem| [mem[:startDate], mem[:endDate]] }.group_by(&:itself).select { |date, seen| seen.count == 1 }.keys.each_slice(2).map do |sd, ed|
+        { item: person.first, itemLabel: person.last, startDate: sd, endDate: ed }
+      end
+    end.flatten
+  end
+
   class Officeholder < OfficeholderBase
     def columns
       %w[no img name start end].freeze
+    end
+
+    def empty?
+      super || startDate < '2008-01-01'
     end
   end
 end
