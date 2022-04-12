82c064c330c6a3a1ba9955648a315ef9fde5ad5c
Tony Bowden <tony@tmtm.com>
2022-04-12 12:30:33 +0300

    update to latest add-p39.js

--- sources/federal-council/add-p39.js
+++ sources/federal-council/add-p39.js
@@ -2,11 +2,23 @@ const fs = require('fs');
 let rawmeta = fs.readFileSync('meta.json');
 let meta = JSON.parse(rawmeta);
 
-module.exports = (id, startdate, enddate) => {
+module.exports = (id, label, startdate, enddate) => {
   qualifier = { }
   if(meta.term) qualifier['P2937'] = meta.term
   if(startdate) qualifier['P580']  = startdate
   if(enddate)   qualifier['P582']  = enddate
+
+  reference = {}
+  if(meta.source) {
+    var wpref = /wikipedia.org/;
+    if (wpref.test(meta.source)) {
+      reference['P4656'] = meta.source
+    } else {
+      reference['P854'] = meta.source
+    }
+  }
+  reference['P813'] = new Date().toISOString().split('T')[0]
+  reference['P1810'] = label
 
   return {
     id,
@@ -14,7 +26,7 @@ module.exports = (id, startdate, enddate) => {
       P39: {
         value: meta.position,
         qualifiers: qualifier,
-        references: { P4656: meta.source }
+        references: reference,
       }
     }
   }
