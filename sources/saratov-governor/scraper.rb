28057dc5499eaf35e0c310ffeca5b379685e2d5b
Tony Bowden <tony@tmtm.com>
2022-05-12 09:51:31 +0300

    compare Governors of Saratov

---
+++ sources/saratov-governor/scraper.rb
@@ -0,0 +1,29 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no img name _ party dates].freeze
+    end
+
+    def raw_combo_date
+      noko.css('i').remove
+      super.gsub('from ', '')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
