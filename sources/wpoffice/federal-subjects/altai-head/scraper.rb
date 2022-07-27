a90e171cb7887660974e80b3c4d4470892a352a1
Tony Bowden <tony@tmtm.com>
2022-07-27 07:31:12 +0300

    update scraper for new layout

--- sources/wpoffice/federal-subjects/altai-head/scraper.rb
+++ sources/wpoffice/federal-subjects/altai-head/scraper.rb
@@ -15,7 +15,7 @@ class OfficeholderList < OfficeholderListBase
 
   class Officeholder < OfficeholderBase
     def columns
-      %w[no img name start end].freeze
+      %w[no img _ name start end].freeze
     end
   end
 end
